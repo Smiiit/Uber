034e897e51cc9668d346db7776a6f8cf4f5623e2
Smiiit <smitkpanchal03@gmail.com>
2025-09-07 20:20:03 +0530

    Route, Controller, Service Completed : User Registration

--- Backend/app.js
+++ Backend/app.js
@@ -1,17 +1,22 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
-const cors = require("cors");
-const connectToDb = require("./db/db.js");
-
 const app = express();
+const cors = require("cors");
 
-connectToDb();
+const connectToDb = require("./db/db.js");
+const userRoutes = require("./routes/user.routes.js");
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+connectToDb();
 
 app.get("/", (req, res) => {
   res.send("Radhe Radhe! Server is up and running.");
 });
+
+app.use("/api/users", userRoutes);
 
 module.exports = app;
